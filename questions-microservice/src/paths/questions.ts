60acd153c2c4b168d6f85b5243709ae9d1e04019
marc armgren <marc_armgren@hotmail.com>
2023-08-16 10:27:12 +0200

    Fix shared types for rn app, alias in node service

--- questions-microservice/src/paths/questions.ts
+++ questions-microservice/src/paths/questions.ts
@@ -1,6 +1,6 @@
 import { ServerRequest, ServerResponse } from "../WebServerInterfaces"
 import { sendEmptySuccess, sendSuccess } from "../ResponseTypesafety"
-import { QuestionInput } from "../../../shared-types/QuestionsResponse"
+import { QuestionInput } from "@/shared-types/QuestionsResponse"
 
 const questions=(req: ServerRequest, res: ServerResponse) => {
     return sendSuccess("a response", res)
